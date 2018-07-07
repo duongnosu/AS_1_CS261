4031a4a60978e8a9b365c8c766b043c90d0ca4e4
Nhu <duongn@oregonstate.edu>
2018-07-07 17:47:43 -0400

    Got Q2 done

--- Assignment_1_Skeleton_Code_Files/Q1.c
+++ Assignment_1_Skeleton_Code_Files/Q1.c
@@ -1,9 +1,9 @@
 /* CS261- Assignment 1 - Q.1*/
-/* Name:
- * Date:
+/* Name:Nhu Duong
+ * Date: July 6th, 2018
  * Solution description:
  */
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -16,14 +16,35 @@ struct student{
 
 struct student* allocate(){
      /*Allocate memory for ten students*/
-     
+     return (stuct student*) malloc(sizeof(struck student)*10);
      /*Return the pointer*/
+
 }
 
+
 void generate(struct student* students){
-     /*Generate random and unique IDs and random scores for ten students, 
-IDs being between 1 and 10, scores between 0 and 100*/
-     
+     /*Generate random and unique IDs and random scores for ten students,
+     IDs being between 1 and 10, scores between 0 and 100*/
+
+          int theid;
+          theid = rand()%10 +1;
+          int arrayid[9];
+          for (int k=0; k<10; k++){
+               arrayid[k]=0;
+          }
+          while (int j<10){
+
+          }
+
+
+     }
+     for (int i=0; i<10; i++){
+          int theid;
+          theid = rand()%10 +1;
+          //Still working on this
+          students[i].id = rand()%10 +1;
+          students[i].score = rand()%101;
+     }
 }
 
 void output(struct student* students){
@@ -36,9 +57,9 @@ void output(struct student* students){
 }
 
 void summary(struct student* students){
-     /*Compute and print the minimum, maximum and average scores of the 
+     /*Compute and print the minimum, maximum and average scores of the
 ten students*/
-     
+
 }
 
 void deallocate(struct student* stud){
@@ -46,18 +67,18 @@ void deallocate(struct student* stud){
 }
 
 int main(){
+     srand(time(NULL));
     struct student* stud = NULL;
-    
+
     /*Call allocate*/
-    
+
     /*Call generate*/
-    
+
     /*Call output*/
-    
+
     /*Call summary*/
-    
+
     /*Call deallocate*/
 
     return 0;
 }
-
--- Assignment_1_Skeleton_Code_Files/Q2.c
+++ Assignment_1_Skeleton_Code_Files/Q2.c
@@ -1,6 +1,6 @@
 /* CS261- Assignment 1 - Q.2*/
-/* Name:
- * Date:
+/* Name:Nhu Duong
+ * Date: July 7th, 2018
  * Solution description:
  */
 #include <stdio.h>
@@ -9,27 +9,45 @@
 
 int foo(int* a, int* b, int c){
     /*Swap the addresses stored in the pointer variables a and b*/
-    
+    int *temp;
+    temp= a;
+    a= b;
+    b=temp;
+
     /*Decrement the value of integer variable c*/
-    
+    c= c-1;
     /*Return c*/
+    return c;
 }
 
 int main(){
     /*Declare three integers x,y and z and initialize them randomly to values in [0,10] */
-    
+    int x, y, z;
+    srand(time(NULL));
+    x= rand()%11;
+    y= rand()%11;
+    z= rand()%11;
+
+
     /*Print the values of x, y and z*/
-    
+    printf("x = %d\n", x);
+    printf("y = %d\n", y);
+    printf("z = %d\n", z);
+    printf("\n" );
     /*Call foo() appropriately, passing x,y,z as parameters*/
-    
+    int fo =foo(&x,&y,z);
     /*Print the values of x, y and z*/
-    
+    printf("x = %d\n", x);
+    printf("y = %d\n", y);
+    printf("z = %d\n", z);
+
     /*Print the value returned by foo*/
- 
-    
+
+    printf("The value return by foo is  %d\n", fo );
+
+
     return 0;
 }
-    
-    
 
-
+//4a, Is the return value different than the value of integer z? Why or why not?
+// The return value is diffrent from the value of z because in foo function, we decrement the value of z and return it after
